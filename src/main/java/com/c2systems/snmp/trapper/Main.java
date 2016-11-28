971c2bf416651d0029b1948be60bd88aa46c0f16
Jesper Steen Steffensen <jsteffensen@gmail.com>
2016-11-28 15:28:06 +0100

    Add Main class

---
+++ src/main/java/com/c2systems/snmp/trapper/Main.java
@@ -0,0 +1,16 @@
+package com.c2systems.snmp.trapper;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.impl.DefaultCamelContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+		/*
+		 *   http://camel.apache.org/walk-through-an-example.html
+		 */
+		CamelContext context = new DefaultCamelContext();
+
+	}
+
+}
